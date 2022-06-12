b1b7541409c4e74310ea6fc09dac9171bf3b400a
armanxid <nurarmana@gmail.com>
2022-06-12 19:29:06 +0700

    Tambah Activity
    - Tambah Activity Input Data Anak
    - Intent dari FAB Home Activity ke Input Data Activity

--- app/src/main/java/com/cc22_ps197/nutrichild/HomeActivity.kt
+++ app/src/main/java/com/cc22_ps197/nutrichild/HomeActivity.kt
@@ -1,11 +1,28 @@
 package com.cc22_ps197.nutrichild
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cc22_ps197.nutrichild.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val addButton = binding.fab
+
+        addButton.setOnClickListener {
+            moveToInputPage()
+        }
+    }
+
+    private fun moveToInputPage() {
+        val intent = Intent(this@HomeActivity, InputDataActivity::class.java)
+        startActivity(intent)
     }
 }
